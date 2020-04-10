ea2974206230b62cf1897c33baee5d96de0a0a66
Nick King <knick98@vt.edu>
2020-04-09 23:43:59 -0400

    Mostly Done, Need to fix timing issue

--- homework4.c
+++ homework4.c
@@ -1,45 +1,84 @@
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 #include "homework4.h"
 
+uint8_t  UARTGetChar (uint32_t moduleInstance);
+bool UARTHasChar(uint32_t moduleInstance);
+bool UARTCanSend(uint32_t moduleInstance);
+
+
+
+void initUART(uint32_t moduleInstance, const eUSCI_UART_ConfigV1 config)
+{
+    UART_initModule(moduleInstance, &config);
+    UART_enableModule(moduleInstance);
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN3,GPIO_PRIMARY_MODULE_FUNCTION);
+}
+
+
+
+
+
+
 int main(void)
 {
     char rChar;
     char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+    char phrase = 0;
 
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
 
+    const eUSCI_UART_ConfigV1 uartConfig =
+    {
+            EUSCI_A_UART_CLOCKSOURCE_SMCLK,
+            78,
+            2,
+            0,
+            EUSCI_A_UART_NO_PARITY,
+            EUSCI_A_UART_LSB_FIRST,
+            EUSCI_A_UART_ONE_STOP_BIT,
+    };
 
-    // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
 
 
     // TODO: Initialize EUSCI_A0
-
-
-    // TODO: Enable EUSCI_A0
-
-
+    initUART(EUSCI_A0_BASE, uartConfig);
+    //UART_ableInterrupt(EUSCI_A0_BASE,EUSCI_A_UART_RECEIVE_INTERRUPT);
     while(1)
     {
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
-
-
+        rChar = UARTGetChar (EUSCI_A0_BASE);
         // TODO: If an actual character was received, echo the character to the terminal AND use it to update the FSM.
         //       Check the transmit interrupt flag prior to transmitting the character.
 
+        if(rChar != -1)
+        {
+            while(!UARTCanSend(EUSCI_A0_BASE));
+            UART_transmitData(EUSCI_A0_BASE, rChar);
+        }
+
+        if(charFSM(rChar) && phrase != COMPLETE)
+            phrase += rChar;
+
+        if(phrase != ONE && phrase != TWO && phrase != THREE && phrase != COMPLETE)
+            phrase = 0;
 
 
         // TODO: If the FSM indicates a successful string entry, transmit the response string.
         //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
         //       Make sure to reset the success variable after transmission.
-
-
+        if(phrase == COMPLETE)
+        {
+            if(UARTCanSend(EUSCI_A0_BASE))
+                UART_transmitData(EUSCI_A0_BASE, *response);
+            phrase = 0;
+        }
     }
 }
 
@@ -53,6 +92,32 @@ bool charFSM(char rChar)
 {
     bool finished = false;
 
+    if(rChar == 2 || rChar == 5 || rChar == 3 || rChar == 4)
+        return true;
 
     return finished;
+}
+
+uint8_t  UARTGetChar (uint32_t moduleInstance)
+{
+    if(UARTHasChar(EUSCI_A0_BASE))
+        return -1;
+    return UART_receiveData(EUSCI_A0_BASE);
+
+}
+
+bool UARTHasChar(uint32_t moduleInstance)
+{
+    if(!UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG))
+        return false;
+    return true;
+
+}
+
+bool UARTCanSend(uint32_t moduleInstance)
+{
+    if(!UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG))
+        return false;
+    return true;
+
 }
--- homework4.h
+++ homework4.h
@@ -1,8 +1,8 @@
 /*
  * homework4.h
  *
- *  Created on:
- *      Author:
+ *  Created on: 4/9/2020
+ *      Author: Nicolas King
  */
 
 #ifndef HOMEWORK4_H_
@@ -13,6 +13,12 @@ void initBoard();
 bool charFSM(char rChar);
 
 // TODO: Define any constants that are local to homework.c using #define
+#define COMPLETE 14
+#define ONE 2
+#define TWO 7
+#define THREE 10
+
+
 
 
 #endif /* HOMEWORK4_H_ */
